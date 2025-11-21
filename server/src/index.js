e40fb8bbd4628c9d784b44e32e71b1da322abcbb
mardavi <dmar@my.centennialcollege.ca>
2025-11-21 16:13:16 -0500

    Backend update for register, forgot pass, and reset pass
    backend hash and token implementation on register, forgot pass, and reset pass with API endpoints

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import morgan from "morgan";
 import passport from "passport";
 import { connectDB } from "./db.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
@@ -11,9 +12,12 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({ origin: process.env.CLIENT_ORIGIN }));
 app.use(passport.initialize());
+app.use("/api/auth", authRoutes)
 
 //Route for testing **Can remove later
 app.get("/", (req, res) => res.send("API Running"));
+
+
 
 const PORT = process.env.PORT || 4000;
 
