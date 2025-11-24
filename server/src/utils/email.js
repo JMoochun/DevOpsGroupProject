5e86e5e5ec47a792bf0606c3e534e842a24929f4
caxb <ca.gbislig@gmail.com>
2025-11-23 22:48:04 -0500

    Update email service

--- server/src/utils/email.js
+++ server/src/utils/email.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-  tls: {
+    tls: {
     rejectUnauthorized: false,
   },
 });
