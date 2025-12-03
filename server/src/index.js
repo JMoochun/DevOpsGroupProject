04ab8a5203502456de62fd86a37815021d28f56b
bacanit <trizha.14@gmail.com>
2025-12-03 11:42:51 -0500

    Setup testing environment

--- server/src/index.js
+++ server/src/index.js
@@ -37,8 +37,11 @@ app.use("/api/users", userRoutes);
 // Test route
 app.get("/", (req, res) => res.send("API Running"));
 
+export default app;
+
 // Start server after DB connection
-const PORT = process.env.PORT || 5000;
-connectDB().then(() => {
-  app.listen(PORT, () => console.log("Server running on port:", PORT));
-});
+ connectDB().then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server running on port: ${PORT}`)
+    );
+  });
